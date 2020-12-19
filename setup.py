e94f45d66ee3ebc37f3a945e176fcc99d074ffa2
Ahmet Nuri Yılmaz <57910847+nyahmet@users.noreply.github.com>
2020-12-19 14:28:55 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
 setup(
   name = 'impytool',      
   packages = ['impytool'],  
-  version = '0.9',      
+  version = '0.9.1',      
   license='MIT',        
   description = 'impytool is an opencv based tool that facilitates some image processing operations',   
   long_description=long_description,
